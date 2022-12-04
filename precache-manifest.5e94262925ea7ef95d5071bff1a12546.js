5aa96b251a87c91089e2908c96ee77d444e15a5d
Leichenko Anna <leichenkoanna@air-leichenko.mynet>
2022-12-04 18:02:43 +0000

    Updates

---
+++ precache-manifest.5e94262925ea7ef95d5071bff1a12546.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec1d7bf7714cd17537fd",
+    "url": "/ania-leichenko/weather/static/js/main.ec1d7bf7.chunk.js"
+  },
+  {
+    "revision": "1e2e905d8bcf6652841e",
+    "url": "/ania-leichenko/weather/static/js/1.1e2e905d.chunk.js"
+  },
+  {
+    "revision": "6c74aa57daaaf511b5b1",
+    "url": "/ania-leichenko/weather/static/js/runtime~main.6c74aa57.js"
+  },
+  {
+    "revision": "1ff96048a39c3da7c51fed185d279ad5",
+    "url": "/ania-leichenko/weather/index.html"
+  }
+];
